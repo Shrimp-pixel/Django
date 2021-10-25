8e17414183bacfe096e3fdd4396553e4fb15a83c
Shrimp-pixel <aleksander.17047665@gmail.com>
2021-10-25 15:39:10 +0300

    test

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,8 +23,8 @@ from mainapp import views as mainapp
 urlpatterns = [
     path('', mainapp.index, name='index'),
     path('contact/', mainapp.contact, name='contact'),
-    path('products/', include('mainapp.urls', namespace='products')),
     path('admin/', admin.site.urls),
+    path('products/', include('mainapp.urls', namespace='products')),
 
 
 ]
